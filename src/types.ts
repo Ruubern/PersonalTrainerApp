4aacc69b5324a2b321925c2b8d9e1fee2397ebb9
Ruubern <rooperouhis@gmail.com>
2025-11-30 17:15:09 +0200

    added types.ts and created types Customer and Training

---
+++ src/types.ts
@@ -0,0 +1,26 @@
+export type Customer = {
+    firstname: string;
+    lastname: string;
+    streetaddress: string;
+    postcode: string;
+    city: string;
+    email: string;
+    phone: string;
+    _links: {
+        self: string;
+        customer: string;
+        trainings: string;
+    }
+}
+
+export type Trainings = {
+    date: Date;
+    duration: number;
+    activity: string;
+    _links: {
+        self: string;
+        training: string;
+        customer: string;
+    }
+
+}
